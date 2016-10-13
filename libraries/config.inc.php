5d98ca7a1fa84ab925b78744a7b1ab97d0615e55
rocean <rocean74@gmai.com>
2016-10-13 20:59:25 +0300

    Διορθώσεις

---
+++ libraries/config.inc.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * File: config.inc.php
+ * Created by rocean
+ * Date: 13/10/16
+ * Time: 19:08
+ *
+ * Ρυθμίσεις mysql 
+ *
+ */
+
+
+
+define('CONNSTR', 'mysql:host=localhost;dbname=OWMP');
+define('DBUSER', 'root');
+define('DBPASS', 'documents2015');
+
+sd
