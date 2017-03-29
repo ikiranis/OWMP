69ff6a6ac36d53cf39318e255fc1d2b906dae3f9
rocean <rocean74@gmai.com>
2017-03-29 22:07:15 +0300

    Διορθώσεις

--- AJAX/restoreDatabase.php
+++ AJAX/restoreDatabase.php
@@ -26,7 +26,7 @@ Page::checkValidAjaxRequest(true);
 $manualPlaylists = MyDB::clearArray(MyDB::getTableArray('manual_playlists', 'table_name', null, null, null, null, null));
 
 // Τα επιλεγμένα tables
-$chozenTables = array('files', 'album_arts', 'music_tags', 'options', 'logs',
+$chozenTables = array('music_tags', 'album_arts', 'files', 'options', 'logs',
     'paths', 'manual_playlists');
 
 // Θέτουμε το array με τα tables που θέλουμε να κάνουμε backup
