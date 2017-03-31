cb5d9bb27e9e30a849750dd37b57b0c7b68e7f96
rocean <rocean74@gmai.com>
2017-04-01 00:23:38 +0300

    Διορθώσεις

--- AJAX/restoreDatabase.php
+++ AJAX/restoreDatabase.php
@@ -39,7 +39,7 @@ if($manualPlaylists) {
 }
 
 $backup->sqlFilePath=OUTPUT_FOLDER;
-$backup->sqlFile='backup_20170401001024.sql';
+$backup->sqlFile='backup_20170401002309.sql';
 
 if ($backup->restoreDatabase()) {
     $jsonArray = array('success' => true);
