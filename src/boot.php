f601c27f3048f6f42a38e0c602e100c87b007fbc
rocean <rocean74@gmai.com>
2017-04-05 23:41:19 +0300

    Διορθώσεις

--- src/boot.php
+++ src/boot.php
@@ -44,8 +44,7 @@ $options->defaultOptions = array(
     array('option_name' => 'icecast_enable', 'option_value' => 'false', 'setting' => 1, 'encrypt' => 0),
     array('option_name' => 'default_language', 'option_value' => 'en', 'setting' => 1, 'encrypt' => 0),
     array('option_name' => 'youtube_api', 'option_value' => 'AIzaSyArMqCdw1Ih1592YL96a2Vdo5sGo6vsS4A', 'setting' => 1, 'encrypt' => 0),
-    array('option_name' => 'play_percentage', 'option_value' => '20', 'setting' => 1, 'encrypt' => 0),
-    array('option_name' => 'paok', 'option_value' => '20', 'setting' => 1, 'encrypt' => 0)
+    array('option_name' => 'play_percentage', 'option_value' => '20', 'setting' => 1, 'encrypt' => 0)
 );
 
 // Τα default progress fields
