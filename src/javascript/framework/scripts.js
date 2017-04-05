5c92df4f9b360e66e7c064f160eda064231b4733
rocean <rocean74@gmai.com>
2017-04-06 01:10:12 +0300

    Testing

--- src/javascript/framework/scripts.js
+++ src/javascript/framework/scripts.js
@@ -3058,21 +3058,8 @@ function uploadFile(files) {
             contentType: false,
             processData: false,
 
-            // Custom XMLHttpRequest
-            xhr: function() {
-                var myXhr = $.ajaxSettings.xhr();
-                if (myXhr.upload) {
-                    // For handling the progress of the upload
-                    // myXhr.upload.addEventListener('progress', function(e) {
-                    //     if (e.lengthComputable) {
-                    //         $('progress').attr({
-                    //             value: e.loaded,
-                    //             max: e.total,
-                    //         });
-                    //     }
-                    // } , false);
-                }
-                return myXhr;
+            success: function (data) {
+                alert('ANEBHKE');
             }
         });
 
