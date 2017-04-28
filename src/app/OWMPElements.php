17f1902dbe95f883a7dba0bac82cc16212d91406
rocean <rocean74@gmai.com>
2017-04-29 01:00:51 +0300

    Σπάσιμο της function showPlaylistWindow σε μικρότερες

--- src/app/OWMPElements.php
+++ src/app/OWMPElements.php
@@ -1698,4 +1698,361 @@ class OWMPElements extends OWMP
 
     }
 
+    // Εμφάνιση του παραθύρου για edit tags
+    static function displayEditTagsWindow($UserGroup)
+    {
+
+
+        if ($UserGroup==1)  // Αν ο χρήστης είναι admin
+            $disabled='no';
+        else $disabled='yes';
+
+        ?>
+        <div id="editTag">
+
+            <?php
+                $tags = new Page();
+                $FormElementsArray = array(
+                    array('name' => 'artist',
+                        'fieldtext' => __('tag_artist'),
+                        'type' => 'text',
+                        'required' => 'no',
+                        'maxlength' => '255',
+                        'disabled' => $disabled,
+                        'value' => null),
+                    array('name' => 'album',
+                        'fieldtext' => __('tag_album'),
+                        'type' => 'text',
+                        'required' => 'no',
+                        'maxlength' => '255',
+                        'disabled' => $disabled,
+                        'value' => null),
+                    array('name' => 'genre',
+                        'fieldtext' => __('tag_genre'),
+                        'type' => 'text',
+                        'required' => 'no',
+                        'maxlength' => '20',
+                        'disabled' => $disabled,
+                        'value' => null),
+                    array('name' => 'year',
+                        'fieldtext' => __('tag_year'),
+                        'type' => 'number',
+                        'required' => 'no',
+                        'disabled' => $disabled,
+                        'value' => null),
+                    array('name' => 'live',
+                        'fieldtext' => __('tag_live'),
+                        'type' => 'select',
+                        'options' => array(
+                            array('value' => '0', 'name' => __('tag_live_official')),
+                            array('value' => '1', 'name' => __('tag_live_live'))
+                        ),
+                        'required' => 'no',
+                        'maxlength' => '1',
+                        'disabled' => $disabled,
+                        'value' => null),
+                    array('name' => 'rating',
+                        'fieldtext' => __('tag_rating'),
+                        'type' => 'range',
+                        'required' => 'no',
+                        'maxlength' => '5',
+                        'min' => '0',
+                        'max' => '5',
+                        'step' => '1',
+                        'ticks' => array(0,1,2,3,4,5),
+                        'disabled' => $disabled,
+                        'value' => '0')
+
+
+
+                );
+                $tags->MakeForm('FormMassiveTags', $FormElementsArray, true);
+            ?>
+
+            <div id="myImage"></div>
+
+            <input type="file" name="uploadFile" id="uploadFile" accept='image/*' onchange="readImage(this.files);">
+
+            <div id="editTagButtons">
+                <input type="button" class="myButton" name="submit" id="submit"
+                       value="<?php echo __('tag_form_submit'); ?>" onclick="editFiles();">
+
+                <input type="button" class="myButton" name="clearEdit" id="clearEdit" value="<?php echo __('search_text_clear'); ?>" onclick="resetFormMassiveTags();">
+
+                <input type="button" class="myButton" name="cancelEdit" id="cancelEdit" value="<?php echo __('search_text_cancel'); ?>" onclick="cancelTheEdit();">
+            </div>
+
+        </div>
+
+        <?php
+    }
+
+    // Εμφάνιση του media select
+    static function displayChooseMediaSelect()
+    {
+        global $mediaKinds;
+
+        ?>
+        <div id="ChooseMediaKind">
+            <select name="mediakind" id="mediakind" onchange="searchPlaylist(0,<?php echo PLAYLIST_LIMIT; ?>, true);">
+                <option value="">
+                    All
+                </option>
+                <?php
+                foreach ($mediaKinds as $kind) {
+                    ?>
+                    <option value="<?php echo $kind; ?>">
+                        <?php echo $kind; ?>
+                    </option>
+
+                    <?php
+                }
+                ?>
+            </select>
+        </div>
+        <?php
+    }
+
+    // Εμφάνιση των στοιχείων επιλογής playlist
+    static function displayChoosePlaylistElements($userID)
+    {
+        ?>
+        <div id="ChoosePlaylist">
+            <form id="formChoosePlaylist">
+                <select name="playlist" id="playlist" >
+                    <option value="">
+                        <?php echo __('choose_playlist'); ?>
+                    </option>
+                    <?php
+
+                    // H λίστα με τις manual playlists
+                    $manualPlaylists = MyDB::getTableArray('manual_playlists', 'id, playlist_name', 'user_id=?', array($userID), null, null, null);
+
+                    foreach ($manualPlaylists as $playlist) {
+                        ?>
+                        <option value="<?php echo $playlist['id']; ?>">
+                            <?php echo  $playlist['playlist_name']; ?>
+                        </option>
+
+                        <?php
+                    }
+                    ?>
+                </select>
+            </form>
+        </div>
+
+        <input type="button" id="playPlaylist" onclick="playPlaylist();" title="<?php echo __('play_file'); ?>">
+        <input type="button" id="insertPlaylistClick" onclick="displayInsertPlaylistWindow();" title="<?php echo __('create_playlist'); ?>">
+        <input type="button" id="deletePlaylistClick" onclick="deletePlaylist();" title="<?php echo __('delete_playlist'); ?>">
+
+        <?php
+
+        Page::getHelp('help_manual_playlists');
+    }
+
+    // Εμφάνιση παραθύρου προσθήκης playlist
+    static function displayInsertPlaylistWindow()
+    {
+        ?>
+        <div id="insertPlaylistWindow">
+            <form id="insertPlaylist" name="insertPlaylist">
+                <input type="text" id="playlistName" name="playlistName">
+                <input type="button" class="myButton PlaylistButton" id="insertPlaylistButton" name="insertPlaylistButton" onclick="createPlaylist();"
+                       value="<?php echo __('create_playlist'); ?>">
+                <input type="button" class="myButton" name="cancelPlaylist" id="cancelPlaylist" value="<?php echo __('search_text_cancel'); ?>" onclick="cancelCreatePlaylist();">
+            </form>
+        </div>
+        <?php
+    }
+
+    // Εμφάνιση του παραθύρου για αναζήτηση
+    static function displaySearchWindow()
+    {
+        $fields=MyDB::getTableFields('music_tags',array('id'));
+
+        if($_SESSION['PlaylistCounter']==0) {
+        ?>
+            <div id="search">
+                <form id="SearchForm" name="SearchForm">
+                    <?php
+
+                    for($counter=1;$counter<6;$counter++) {
+
+                        ?>
+                        <div id="searchRow<?php echo $counter; ?>">
+                            <label for="search_field<?php echo $counter; ?>">
+                                <select class="search_field" name="search_field<?php echo $counter; ?>" id="search_field<?php echo $counter; ?>">
+                                    <?php
+                                    foreach ($fields as $field) {
+                                        ?>
+                                        <option value="<?php echo $field; ?>">
+                                            <?php
+                                            switch ($field) {
+                                                case 'song_name': echo __('tag_title'); break;
+                                                case 'artist': echo __('tag_artist'); break;
+                                                case 'genre': echo __('tag_genre'); break;
+                                                case 'date_added': echo __('tag_date_added'); break;
+                                                case 'play_count': echo __('tag_play_count'); break;
+                                                case 'date_last_played': echo __('tag_date_played'); break;
+                                                case 'rating': echo __('tag_rating'); break;
+                                                case 'album': echo __('tag_album'); break;
+                                                case 'video_height': echo __('tag_video_height'); break;
+                                                case 'filesize': echo __('tag_filesize'); break;
+                                                case 'video_width': echo __('tag_video_width'); break;
+                                                case 'track_time': echo __('tag_track_time'); break;
+                                                case 'song_year': echo __('tag_year'); break;
+                                                case 'live': echo __('tag_live'); break;
+                                                case 'album_artwork_id': echo __('tag_album_artwork_id'); break;
+                                            }
+                                            ?>
+                                        </option>
+
+                                        <?php
+                                    }
+                                    ?>
+                                </select>
+                            </label>
+
+                            <select class="search_equality" name="search_equality<?php echo $counter; ?>" id="search_equality<?php echo $counter; ?>">
+
+                                <option value="equal">
+                                    <?php echo __('search_equal'); ?>
+                                </option>
+
+                                <option value="greater">
+                                    <?php echo __('search_greater'); ?>
+                                </option>
+
+                                <option value="less">
+                                    <?php echo __('search_less'); ?>
+                                </option>
+
+
+                            </select>
+
+                            <label for="search_text<?php echo $counter; ?>">
+                                <input type="text" name="search_text<?php echo $counter; ?>" id="search_text<?php echo $counter; ?>">
+                            </label>
+
+                            <select class="search_operator" name="search_operator<?php echo $counter; ?>" id="search_operator<?php echo $counter; ?>">
+
+                                <option value="OR">
+                                    <?php echo __('search_or'); ?>
+                                </option>
+
+                                <option value="AND">
+                                    <?php echo __('search_and'); ?>
+                                </option>
+
+                            </select>
+                        </div>
+
+                        <?php
+                    }
+                    ?>
+
+                    <div id="searchButtons">
+                        <input type="button" class="myButton" name="searching" id="searching"
+                               value="<?php echo __('search_text_search'); ?>" onclick="searchPlaylist(0,<?php echo PLAYLIST_LIMIT; ?>, true);">
+
+                        <input type="button" class="myButton" name="duplicates" id="duplicates"
+                               value="<?php echo __('search_text_duplicates'); ?>" onclick="findDuplicates(0,<?php echo PLAYLIST_LIMIT; ?>, true);">
+
+                        <input type="button" class="myButton" name="playedQueue" id="playedQueue"
+                               value="<?php echo __('search_text_played_queue'); ?>" onclick="loadPlayedQueuePlaylist();">
+
+                        <input type="button" class="myButton" name="clearSearch" id="clearSearch"
+                               value="<?php echo __('search_text_clear'); ?>" onclick="reset();">
+
+                        <input type="button" class="myButton" name="cancelSearch" id="cancelSearch"
+                               value="<?php echo __('search_text_cancel'); ?>" onclick="cancelTheSearch();" >
+                    </div>
+                </form>
+            </div>
+
+            <?php
+        } else {
+        ?>
+            <div id="search"></div>
+            <?php
+        }
+        ?>
+
+
+        <script type="text/javascript">
+
+            // περνάει στην javascript τα options των αντίστοιχων select
+            var liveOptions = <?php echo json_encode([__('tag_live_official'),__('tag_live_live')]); ?>;
+
+            var ratingOptions = <?php echo json_encode([0,1,2,3,4,5]); ?>;
+
+        </script>
+
+        <?php
+    }
+
+    // Εμφάνιση διάφορων εργαλείων
+    static function displaySomeTools($UserGroup)
+    {
+        ?>
+        <div id="someTools">
+            <?php
+            if ($UserGroup==1) {
+                ?>
+                <input type="button" class="myButton" name="sendToJukebox" id="sendToJukebox"
+                       value="<?php echo __('send_to_jukebox'); ?>" onclick="sendToJukeboxList();">
+                <?php
+            }
+            ?>
+        </div>
+        <?php
+    }
+
+    // Εμφάνιση των edit buttons
+    static function displayEditButtons($UserGroup)
+    {
+        if($UserGroup==1) {
+            ?>
+            <div id="editButtons">
+                <input type="button" class="delete_button playlist_button_img"
+                       title="<?php echo __('delete_file'); ?>"
+                       onclick="deleteFile(0);">
+                <input type="button" class="edit_button playlist_button_img"
+                       title="<?php echo __('edit_file'); ?>"
+                       onclick="openMassiveTagsWindow();" >
+                <input type="button" class="export_button playlist_button_img"
+                       title="<?php echo __('export_playlist'); ?>"
+                       onclick="exportPlaylist();" >
+
+            </div>
+
+            <?php
+        }
+    }
+
+    // Εμφάνιση του playlist container
+    static function displayPlaylistContainer($offset,$step)
+    {
+        ?>
+        <div id="playlist_container">
+
+            <?php
+            if($_SESSION['PlaylistCounter']==0) {
+                $_SESSION['condition']=null;   // Μηδενίζει το τρέχον search query
+                $_SESSION['arrayParams']=null;
+                OWMPElements::getPlaylist(null,$offset,$step,null,null,null,null,false);
+            }
+            else {
+                ?>
+
+                <div id="playlist_content"></div>
+
+                <?php
+            }
+
+            ?>
+        </div>
+        <?php
+    }
+
 }
