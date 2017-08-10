d77a29505df35595058d4d4d8d4ceefa97762290
rocean <rocean74@gmai.com>
2017-08-10 17:43:46 +0300

    Προσθήκη σχολίων

--- src/javascript/app/_progressAnimation.js
+++ src/javascript/app/_progressAnimation.js
@@ -29,22 +29,32 @@ var ProgressAnimation =
     // Αρχίζει το progress animation
     init: function(doProgress)
     {
-        // Χρησιμοποιώ το bind(this) αλλιώς δεν παιρνάει το this στο setInterval και requestAnimationFrame
+        // Χρησιμοποιώ το bind(this) αλλιώς δεν περνάει το this στο setInterval και requestAnimationFrame
+        // reference @ https://stackoverflow.com/questions/19459449/running-requestanimationframe-from-within-a-new-object
         this.drawAnimationImage = this.drawAnimationImage.bind(this);
         this.frameDelay = this.frameDelay.bind(this);
+
+        this.x = 0;
 
         // Δημιουργεί το o-progressAnimation μέσα στο #o-progressAnimation_container
         this.initCanvasElement('o-progressAnimation', '#o-progressAnimation_container');
 
+        // Αρχικοποίηση του canvas
         this.canvas = document.querySelector("#o-progressAnimation");
         this.ctx = this.canvas.getContext('2d');
 
+        // Αρχικοποίηση των images για τα frames
         this.initImages();
 
         // Καθαρισμός των τρέχοντων animations
         this.clearAnimations();
 
+        // Έναρξη των animation
+
+        // Τα καρέ του animation του sprite. Χρησιμοποιείται η setInterval γιατί θέλουμε
+        // πολύ μεγαλύτερη καθυστέρηση ανάμεσα στα frames
         this.currentFrameInterval = setInterval(this.frameDelay, 150);
+        // Η μετακίνηση του sprite
         this.imageAnimation = requestAnimationFrame(this.drawAnimationImage);
     },
 
@@ -79,6 +89,7 @@ var ProgressAnimation =
         this.ctx.fillText(this.calculateProgressPercent() + '%', this.x, 10);
     },
 
+    // Επιστρέφει το ποσοστό της θέσης στην οποία βρίσκεται το sprite πάνω στο canvas
     calculateProgressPercent: function()
     {
         return ( (this.x*100)/this.canvas.width ).toFixed(2);
@@ -92,15 +103,17 @@ var ProgressAnimation =
 
         canvasContainerElement = document.querySelector(canvasContainer);
 
+        // Τα properties του canvas
         canvasElement = document.createElement('canvas');
         canvasElement.setAttribute('id', elementName);
         canvasElement.setAttribute('width', canvasContainerElement.offsetWidth);
         canvasElement.setAttribute('height', canvasContainerElement.offsetHeight);
 
+        // Το προσθέτει μέσα στο canvas container div
         document.querySelector(canvasContainer).appendChild(canvasElement);
     },
 
-    // Καθαρισμός των τρέχοντων interval
+    // Καθαρισμός των τρέχοντων animations
     clearAnimations: function()
     {
         clearInterval(this.currentFrameInterval);
@@ -139,19 +152,25 @@ var ProgressAnimation =
     // Σχεδιάζει το τρέχον frame
     drawAnimationImage: function()
     {
+        // σβήσιμο των περιεχομένων του canvas
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+        // Επανασχεδίαση των περιεχομένων του canvas
+
+        // Σχεδίαση του image
         this.ctx.drawImage(this.animationImages[this.currentFrame], this.x, 0, 70, 70);
 
         // if(doProgress) { // Αν είναι true το doProgress σχεδιάζει την progress bar
         //     drawProgressBar();
         // }
 
-        this.drawProgressBar();
-        this.drawProgressText();
+        this.drawProgressBar();  // Σχεδίαση της progress bar
+        this.drawProgressText();  // Σχεδίαση του κειμένου που θα εμφανιστεί
 
+        // Υπολογισμός της νέας θέσης του x
         this.calculateX();
 
+        // Loop για το επόμενο frame
         this.imageAnimation = requestAnimationFrame(this.drawAnimationImage);
     }
 
