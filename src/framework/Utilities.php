189beccd06977c8c649112ea7b5c0c29df89dce3
rocean <rocean74@gmai.com>
2017-12-07 01:50:15 +0200

    test

--- src/framework/Utilities.php
+++ src/framework/Utilities.php
@@ -332,7 +332,6 @@ class Utilities
      */
     static function checkWebServerForRoutingRules()
     {
-        trigger_error($_SERVER['SERVER_SOFTWARE']);
         // Έλεγχος για το τι server είναι. Αν δεν είναι lighttpd, τρέχει τον έλεγχο για apache
         if(isset($_SERVER['SERVER_SOFTWARE'])) {
             if (stripos($_SERVER['SERVER_SOFTWARE'], 'Apache') !== false) {
@@ -344,7 +343,7 @@ class Utilities
 
             // Έλεγχος αν λειτουργεί το htaccess γενικά
             if (!Utilities::checkIfHTaccessWorks()) {
-                die(__('htaccess_cant_work'));
+                die($_SERVER['SERVER_SOFTWARE']. __('htaccess_cant_work'));
             }
         }
     }
