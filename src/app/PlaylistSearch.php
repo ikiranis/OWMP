e15ef019e2b6c49265ccca421d364a5fb7a2b44e
rocean <rocean74@gmai.com>
2017-12-13 22:43:38 +0200

    Διόρθωση αναζήτησης διπλοεγγραφών

--- src/app/PlaylistSearch.php
+++ src/app/PlaylistSearch.php
@@ -484,7 +484,7 @@ class PlaylistSearch extends OWMPElements
 
         // Κάνει join την $tempUserPlaylist με τα music_tags και files για εμφάνιση της playlist
         $this->playlist = MyDB::getTableArray($this->mainTables, 'music_tags.*, files.path, files.filename, files.hash, files.kind',
-            null, null, 'files.hash ASC LIMIT ' . $this->offset . ',' . $this->step, $this->tempUserPlaylist, $this->joinFieldsArray);
+            null, null, ' LIMIT ' . $this->offset . ',' . $this->step, $this->tempUserPlaylist, $this->joinFieldsArray);
 
     }
 
