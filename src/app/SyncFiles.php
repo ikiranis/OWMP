483a08d77f4fba34263d17b350aa88040db2dbb8
rocean <rocean74@gmail.com>
2018-02-09 01:38:31 +0200

    Remove timezone

--- src/app/SyncFiles.php
+++ src/app/SyncFiles.php
@@ -848,8 +848,6 @@ class SyncFiles
     public function syncTheFiles($mediakind) {
         set_time_limit(0);
         ini_set('memory_limit','1024M');
-        // Set Athens timezone
-        date_default_timezone_set('Europe/Athens');
 
         Progress::setLastMomentAlive(false);
 
--- src/framework/Page.php
+++ src/framework/Page.php
@@ -175,9 +175,6 @@ class Page
 
         <?php
 
-        // Set Athens timezone
-        date_default_timezone_set('Europe/Athens');
-
 
     }
 
