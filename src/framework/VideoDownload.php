a92332a38e4948e966b5c7200e2a49e609e69b9f
rocean <rocean74@gmail.com>
2021-10-26 00:37:48 +0300

    Improve aria2c args

--- src/framework/VideoDownload.php
+++ src/framework/VideoDownload.php
@@ -134,7 +134,7 @@ class VideoDownload
         // το όνομα του αρχείου που θα κατεβάσει με το full path
         $outputfilename = shell_exec('youtube-dl ' . $youtubedlDefaultOptions . ' --get-filename -f '.$downloadString);
 
-        $ariaDownloadString = ' --external-downloader aria2c --external-downloader-args "-j 8 -s 8 -x 8 -k 5M"';
+        $ariaDownloadString = ' --external-downloader aria2c --external-downloader-args "-j 16 -s 16 -x 16 -k 1M"';
 
         error_log('youtube-dl ' . $youtubedlDefaultOptions . ' -f '.$downloadString . $ariaDownloadString);
 
